3e2ef4198445b0ed00c10673b607ffb012430a08
Moses Akor <gabrielmosesakor@gmail.com>
2020-12-26 18:44:45 +0100

    configured fontawesome

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   watch: false,
@@ -16,3 +17,7 @@ module.exports = {
     ],
   },
 };
+
+plugins: [
+  new FaviconsWebpackPlugin('/path/to/logo.png') // svg works too!
+]

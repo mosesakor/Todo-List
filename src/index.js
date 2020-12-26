8313f68cdc3fdb90e55a8b05b6f846a1131aa025
Moses Akor <gabrielmosesakor@gmail.com>
2020-12-26 19:15:34 +0100

    added styling for new task button

--- src/index.js
+++ src/index.js
@@ -3,21 +3,12 @@ import './style.css';
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
- element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
 
 const createTodoObject = (title, description, date) => {
   
+}
+
+const Dom = () => {
+
 }
 

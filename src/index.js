80eecc7db9cca974ede2e7e41f4ec912d31d0b26
Moses Akor <gabrielmosesakor@gmail.com>
2020-12-27 16:11:41 +0100

    set up new project button

--- src/index.js
+++ src/index.js
@@ -4,13 +4,37 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 
-const createTodoObject = (title, description, date) => {
-  
+const createTodoObject = () => {
+
+  const getUserInput = () => {
+    return Object.values(Array.from(document.querySelectorAll('input')).reduce((a, input) => ({...a, [input.id]: input.value}), {}))
+  };
+
+  const userInput = getUserInput;
+
+  const todoObject = (title, details, date) => {
+
+  }
+
+};
+
+const project = () => {
+  const projects = {}
+
+  projects["Default Project"] = "la"
 }
 
+
+
 const Dom = () => {
-  const newTask = document.getElementById('newTask');
-  newTask.onclick = openForm();
+
+  const openBtn = document.getElementById("openBtn");
+  const closeBtn = document.getElementById("closeBtn");
+  const addTask = document.getElementById("addTask");
+
+  openBtn.onclick = openForm;
+  closeBtn.onclick = closeForm;
+  addTask.onclick;
 
   function openForm() {
     document.getElementById("myForm").style.display = "block";
@@ -23,5 +47,14 @@ const Dom = () => {
   function resetForm() {
     document.getElementById("form").reset()
   };
+
+  return {}
 }
+
+Dom();
+getUserInput.input();
+
+
+
+
 
